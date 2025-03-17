cff0df28651c1b4fd15b1868fab82be71e938245
definitelyna <nguyentrongnamanh@gmail.com>
2025-03-17 23:24:35 +0700

    Shit ton of changes

--- src/pages/QuestionCheck/QuestionCheck.tsx
+++ src/pages/QuestionCheck/QuestionCheck.tsx
@@ -1,6 +1,8 @@
 // import { useEffect, useState } from "react";
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import "./QuestionCheck.css";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 
 const QuestionCheck = () => {
   const questions = [
@@ -43,32 +45,135 @@ const QuestionCheck = () => {
   // }, []);
 
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        bgcolor: "#f4eac6",
+        width: "100%",
+      }}
+    >
       <Box
         sx={{
-          height: "100vh",
-          width: "70vw",
+          height: "auto",
+          width: "67vw",
           color: "#a40f33",
-          bgcolor: "#f4f3ec",
+          bgcolor: "#fcf8ed",
           display: "flex",
           flexDirection: "column",
-          justifyContent: "space-around",
-          pl: 3,
+          alignItems: "center",
+          justifyContent: "space-between",
+          px: 3,
         }}
       >
-        {questions.map((question) => (
-          <Box key={question.id}>
-            <Box sx={{ display: "flex", flexDirection: "row" }}>
-              <h2>{question.question}</h2>
-              {Array.from({ length: question.star_rating }, (_, i) => (
-                <div key={i}>⭐</div>
-              ))}
+        <Box sx={{ textAlign: "right", mt: 3 }}>
+          <h1 style={{ fontSize: 60, marginLeft: "auto" }}>
+            MINI CS50x PUZZLE DAY
+          </h1>
+          <h3 style={{ fontWeight: "normal" }}>By STEM Society</h3>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "100%",
+            mt: 7,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              width: "60%",
+              px: 5,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignSelf: "flex-end",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <b>Guess attempts</b>
+              <Box>23</Box>
             </Box>
 
-            <TextField />
-            <p>Answered: {question.answered ? "Yes" : "No"}</p>
+            {questions.map((question) => (
+              <Box
+                key={question.id}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "left",
+                  mb: 7,
+                }}
+              >
+                <Box sx={{ display: "flex", flexDirection: "row" }}>
+                  <h2 style={{ fontWeight: "normal" }}>{question.question}</h2>
+                  {Array.from({ length: question.star_rating }, (_, i) => (
+                    <div key={i}>
+                      <Box
+                        component="img"
+                        src="/public/rubber-duck.png"
+                        sx={{ height: "4vh", ml: 0.5 }}
+                      ></Box>
+                    </div>
+                  ))}
+                </Box>
+
+                <Box sx={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                  <TextField
+                    sx={{
+                      height: 40,
+                      "& .MuiInputBase-root": {
+                        height: 40,
+                        bgcolor: question.answered ? "green" : "white",
+                      },
+                      width: "70%",
+                    }}
+                    placeholder="Answer"
+                    disabled={question.answered}
+                  />
+
+                  {question.answered ? (
+                    <LockOpenOutlinedIcon />
+                  ) : (
+                    <LockOutlinedIcon />
+                  )}
+                </Box>
+
+                <Button
+                  sx={{
+                    bgcolor: "#a40f33",
+                    color: "#fcf8ed",
+                    mt: 2,
+                    width: "20%",
+                  }}
+                >
+                  Submit
+                </Button>
+              </Box>
+            ))}
           </Box>
-        ))}
+
+          <Box
+            sx={{
+              width: "30%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <b>Leaderboard</b>
+
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
